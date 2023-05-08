49b4348d06b4984157a64696fdd8e39255b54519
saguoran <reesadness122@gmail.com>
2023-05-08 14:02:28 -0400

    updated names

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes, A } from "@solidjs/router";
 import logo from './assets/gundam.png';
 import Home from './pages/Home';
-
-const phone = '6031112298';
+const companyName = 'One General Construction';
+const phone = '6477401168';
 const email = '';
-const location = '1750 Finch Ave E, North York, ON M2J 2X5';
+const location = '47 Thorncliffe Park Dr, East York, ON M4H 1J5';
 function App() {
   return (
     <>
@@ -12,7 +12,7 @@ function App() {
         <div class="navbar-brand">
           <A class="navbar-item" href="/">
             <img src={logo} width="28" height="28"> </img>
-            <span class="title is-4 ml-1">Gundam</span>
+            <span class="title is-4 ml-1">{companyName}</span>
           </A>
         </div>
         <div class="navbar-menu is-active">
@@ -46,13 +46,13 @@ function App() {
               <img src={logo} />
             </p>
           </figure>
-            <h3 class='title is-1 mx-2'>Gundam</h3>
+            <h3 class='title is-3 mx-2'>{companyName}</h3>
           
         </article>
         <div class="content">
 
           <p className='pt-3'>
-            Welcome to <strong>Gundam Inc</strong>, where we are passionate about providing our customers with the highest quality products and services in the technology industry. Our team of engineers and developers is constantly pushing the boundaries of innovation, and we are dedicated to helping our clients stay ahead of the curve.
+            Welcome to <strong>{companyName}</strong>, where we are passionate about providing our customers with the highest quality products and services in the technology industry. Our team of engineers and developers is constantly pushing the boundaries of innovation, and we are dedicated to helping our clients stay ahead of the curve.
           </p>
 
           <div class="icon-text">
@@ -76,12 +76,13 @@ function App() {
             <a href="https://www.google.com/maps?q=1750+Finch+Ave+E,+North+York,+ON+M2J+2X5">
               <span>{location}</span>
             </a>
-            <iframe src="https://maps.google.com/maps?q=1750+Finch+Ave+E,+North+York,+ON+M2J+2X5&output=embed" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%201%20Grafton%2047%20Thorncliffe%20Park%20Dr,%20East%20York,%20ON%20M4H%201J5Street+(One%20General%20Construction)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
 
         </div>
         <p>
-          Copyright &copy; 2023 By Gundam Inc
+          COPYRIGHT &copy; 2023 {companyName}<br/>
+          All content, trade names and/or trade dress, trademarks, artwork and associated imagery are trademarks and/or copyright material of their respective owners. All rights reserved. 
         </p>
       </footer>
     </>
