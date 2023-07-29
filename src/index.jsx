8217aa92750ff854e713a71e6294ac08ce7a3971
saguoran <reesadness122@gmail.com>
2023-07-29 13:30:36 -0400

    updated meta tags

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import { render } from 'solid-js/web';
 import './index.css';
 import { Router } from "@solidjs/router";
 import App from './App';
-
+import og_image from './assets/ogc-og_img.jpg';
 import logo from "./assets/logo2.png";
-
+const og_url =  "https://ogc-web-30135.web.app";
 render(() => (
   <Router>
 
@@ -15,23 +15,31 @@ render(() => (
 );
 render(() => (
   <>
-    <meta charset="utf-8" />
-    <title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</title>
-    <meta name="description" content="One Gene
-ral Construction specializes in flagstone installations, interlock driveways, landscaping, flower boxes, sod installation, power washing, and sealing services. Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs."/>
-    <meta name="keywords" content="One General Construction, flagstone, interlock driveways, landscaping, flower boxes, sod installation, power wash, sealing services, hardscaping" />
-    <meta property="og:title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
-    <meta property="og:description" content="One General Construction specializes in flagstone installations, interlock driveways, landscaping, flower boxes, sod installation, power washing, and sealing services. Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
-    <meta property="og:image" content="https://www.onegeneralconstruction.com/images/og-image.jpg" />
-    <meta property="og:url" content="https://www.onegeneralconstruction.com/services" />
-    <link rel="canonical" href="https://www.onegeneralconstruction.com/services" />
+{/* <!-- Primary Meta Tags --> */}
+<title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</title>
+<meta name="title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
+<meta name="description" content="specializes in flagstone installations, interlock driveways, landscaping, flower boxes, sod installation, power washing, and sealing services. Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
 
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+{/* <!-- Open Graph / Facebook --> */}
+<meta property="og:type" content="website" />
+<meta property="og:url" content={og_url} />
+<meta property="og:title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
+<meta property="og:description" content="specializes in flagstone installations, interlock driveways, landscaping, flower boxes, sod installation, power washing, and sealing services. Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
+<meta property="og:image" content={og_image} />
 
-    <meta name="theme-color" content="#ffffff" />
-    <link rel="shortcut icon" type="image/ico" href={logo} />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
-    <title>One General Construction</title>
+{/* <!-- Twitter --> */}
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content={og_url} />
+<meta property="twitter:title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
+<meta property="twitter:description" content="specializes in flagstone installations, interlock driveways, landscaping, flower boxes, sod installation, power washing, and sealing services. Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
+<meta property="twitter:image" content={og_image}  />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<meta name="theme-color" content="#ffffff" />
+<link rel="shortcut icon" type="image/ico" href={logo} />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+<title>One General Construction</title>
   </>
 ), document.getElementById('meta')
 );
