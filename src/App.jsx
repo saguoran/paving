c83bd32b67b86b63fba4533eabd1b420e34392cb
saguoran <reesadness122@gmail.com>
2023-07-29 20:53:42 -0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
 import { Route, Routes, A } from "@solidjs/router";
-import logo from './assets/logo.png';
+// import logo from './assets/logo.png';
 import Home from './pages/Home';
 import Service from './pages/Services';
 import PageWrapper from "./components/PageWrapper";
-// import { baseUrl } from './../vite.config';
-// console.log()
+
+
+import logo from "./assets/logo2.png";
+const og_url =  "https://ogc-web-30135.web.app";
+import og_image from './assets/ogc-og_img.jpg';
+
 export const baseUrl = '/';
 const companyName = 'One General Construction';
 const phone = '6477401168';
 const email = 'OneGeneralConstruction@gmail.com';
 const location = '47 Thorncliffe Park Dr, East York, ON M4H 1J5';
+const title = "One General Construction - Flagstone, Interlock, Landscaping, Power Wash";
+const description = "Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs.";
+
+import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
 
 function App() {
   return (
+    <>
+    <MetaProvider>
+    <div class="Home">
+      <Title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</Title>
+      <Meta name="title" content={title} />
+      <Meta name="description" content={description} />
+
+       {/* <!-- Open Graph / Facebook --> */}
+  <Meta property="og:type" content="website" />
+  <Meta property="og:url" content={og_url} />
+  <Meta property="og:title" content={title} />
+  <Meta property="og:description" content={description} />
+  <Meta property="og:image" content={og_image} />
+
+ {/* <!-- Twitter --> */}
+ <Meta property="twitter:card" content={og_image} />
+ <Meta property="twitter:url" content={og_url} />
+ <Meta property="twitter:title" content={title} />
+ <Meta property="twitter:description" content={description} />
+ <meta property="twitter:image" content={og_image}/>
+    </div>
+  </MetaProvider>
       <Routes >
         <Route path={baseUrl} component={PageWrapper} >
           <Route path="/" component={Home} />
@@ -20,7 +50,7 @@ function App() {
           <Route path="/about" component={() => <div>about</div>} />
         </Route>
       </Routes>
-    
+      </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,45 +3,15 @@ import { render } from 'solid-js/web';
 import './index.css';
 import { Router } from "@solidjs/router";
 import App from './App';
-import og_image from './assets/ogc-og_img.jpg';
-import logo from "./assets/logo2.png";
-const og_url =  "https://ogc-web-30135.web.app";
+
+
 render(() => (
+  <>
   <Router>
 
     <App />
   </Router>
+  </>
 ), document.getElementById('root')
 );
-// render(() => (
-//   <>
-//   <meta charset="utf-8" />
-// <meta name="viewport" content="width=device-width, initial-scale=1" />
-// <link rel="shortcut icon" type="image/ico" href={logo} />
-// <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
-
-
-// {/* <!-- Primary Meta Tags --> */}
-// <title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</title>
-// <meta name="title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
-// <meta name="description" content="Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
-
-// {/* <!-- Open Graph / Facebook --> */}
-// <meta property="og:type" content="website" />
-// <meta property="og:url" content="https://www.heymeta.com/url/metatags.io" />
-// <meta property="og:title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
-// <meta property="og:description" content="Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
-// <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
-
-// {/* <!-- Twitter --> */}
-// <meta property="twitter:card" content="summary_large_image" />
-// <meta property="twitter:url" content="https://www.heymeta.com/url/metatags.io" />
-// <meta property="twitter:title" content="One General Construction - Flagstone, Interlock, Landscaping, Power Wash" />
-// <meta property="twitter:description" content="Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs." />
-// <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
-
-//   </>
-// ), document.getElementById('meta')
-// );
-
 
