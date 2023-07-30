097f32d7a9bfea76b9f5f1d5546a58a6704bd27e
saguoran <reesadness122@gmail.com>
2023-07-29 21:18:29 -0400

    test html only

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Service from './pages/Services';
 import PageWrapper from "./components/PageWrapper";
 
+import { Router } from "@solidjs/router";
 
 import logo from "./assets/logo2.png";
 const og_url = "https://ogc-web-30135.web.app";
@@ -17,36 +18,40 @@ const location = '47 Thorncliffe Park Dr, East York, ON M4H 1J5';
 const title = "One General Construction - Flagstone, Interlock, Landscaping, Power Wash";
 const description = "Our expert team creates beautiful outdoor spaces and ensures long-lasting durability. Contact us for all your landscaping and hardscaping needs.";
 
-import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
-
+//import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
+//<MetaProvider>
+//<div class="Home">
+  //<Meta charset="utf-8" />
+  //<Meta name="viewport" content="width=device-width, initial-scale=1" />
+  //<Link rel="shortcut icon" type="image/ico" href={logo} />
+  //<Link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+  //<Title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</Title>
+  //<Meta name="title" content={title} />
+  //<Meta name="description" content={description} />
+  //{/* <!-- Google / Search Engine Tags --> */}
+  //<Meta itemprop="name" content={title} />
+  //<Meta itemprop="description"  content={description}/>
+  //<Meta itemprop="image" content={og_image}/>
+  //{/* <!-- Open Graph / Facebook --> */}
+  //<Meta property="og:type" content="website" />
+  //<Meta property="og:url" content={og_url} />
+  //<Meta property="og:title" content={title} />
+  //<Meta property="og:description" content={description} />
+  //<Meta property="og:image" content={og_image} />
+  //{/* <!-- Twitter --> */}
+  //<Meta property="twitter:card" content={og_image} />
+  //<Meta property="twitter:url" content={og_url} />
+  //<Meta property="twitter:title" content={title} />
+  //<Meta property="twitter:description" content={description} />
+  //<Meta property="twitter:image" content={og_image} />
+//</div>
+//</MetaProvider>
 function App() {
   return (
     <>
-      <MetaProvider>
-        <div class="Home">
-          <Meta charset="utf-8" />
-          <Meta name="viewport" content="width=device-width, initial-scale=1" />
-          <Link rel="shortcut icon" type="image/ico" href={logo} />
-          <Link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
-          <Title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</Title>
-          <Meta name="title" content={title} />
-          <Meta name="description" content={description} />
 
-          {/* <!-- Open Graph / Facebook --> */}
-          <Meta property="og:type" content="website" />
-          <Meta property="og:url" content={og_url} />
-          <Meta property="og:title" content={title} />
-          <Meta property="og:description" content={description} />
-          <Meta property="og:image" content={og_image} />
+  <Router>
 
-          {/* <!-- Twitter --> */}
-          <Meta property="twitter:card" content={og_image} />
-          <Meta property="twitter:url" content={og_url} />
-          <Meta property="twitter:title" content={title} />
-          <Meta property="twitter:description" content={description} />
-          <meta property="twitter:image" content={og_image} />
-        </div>
-      </MetaProvider>
       <Routes >
         <Route path={baseUrl} component={PageWrapper} >
           <Route path="/" component={Home} />
@@ -54,6 +59,7 @@ function App() {
           <Route path="/about" component={() => <div>about</div>} />
         </Route>
       </Routes>
+  </Router>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 import './index.css';
-import { Router } from "@solidjs/router";
 import App from './App';
 
 
 render(() => (
-  <>
-  <Router>
-
-    <App />
-  </Router>
-  </>
+    App
 ), document.getElementById('root')
 );
 
