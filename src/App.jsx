0c13f18317dfab7f4e30370e463f6880ffbb9e38
saguoran <reesadness122@gmail.com>
2023-07-29 20:59:57 -0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import PageWrapper from "./components/PageWrapper";
 
 
 import logo from "./assets/logo2.png";
-const og_url =  "https://ogc-web-30135.web.app";
+const og_url = "https://ogc-web-30135.web.app";
 import og_image from './assets/ogc-og_img.jpg';
 
 export const baseUrl = '/';
@@ -22,27 +22,31 @@ import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
 function App() {
   return (
     <>
-    <MetaProvider>
-    <div class="Home">
-      <Title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</Title>
-      <Meta name="title" content={title} />
-      <Meta name="description" content={description} />
+      <MetaProvider>
+        <div class="Home">
+          <Meta charset="utf-8" />
+          <Meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Link rel="shortcut icon" type="image/ico" href={logo} />
+          <Link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+          <Title>One General Construction - Flagstone, Interlock, Landscaping, Power Wash</Title>
+          <Meta name="title" content={title} />
+          <Meta name="description" content={description} />
 
-       {/* <!-- Open Graph / Facebook --> */}
-  <Meta property="og:type" content="website" />
-  <Meta property="og:url" content={og_url} />
-  <Meta property="og:title" content={title} />
-  <Meta property="og:description" content={description} />
-  <Meta property="og:image" content={og_image} />
+          {/* <!-- Open Graph / Facebook --> */}
+          <Meta property="og:type" content="website" />
+          <Meta property="og:url" content={og_url} />
+          <Meta property="og:title" content={title} />
+          <Meta property="og:description" content={description} />
+          <Meta property="og:image" content={og_image} />
 
- {/* <!-- Twitter --> */}
- <Meta property="twitter:card" content={og_image} />
- <Meta property="twitter:url" content={og_url} />
- <Meta property="twitter:title" content={title} />
- <Meta property="twitter:description" content={description} />
- <meta property="twitter:image" content={og_image}/>
-    </div>
-  </MetaProvider>
+          {/* <!-- Twitter --> */}
+          <Meta property="twitter:card" content={og_image} />
+          <Meta property="twitter:url" content={og_url} />
+          <Meta property="twitter:title" content={title} />
+          <Meta property="twitter:description" content={description} />
+          <meta property="twitter:image" content={og_image} />
+        </div>
+      </MetaProvider>
       <Routes >
         <Route path={baseUrl} component={PageWrapper} >
           <Route path="/" component={Home} />
@@ -50,7 +54,7 @@ function App() {
           <Route path="/about" component={() => <div>about</div>} />
         </Route>
       </Routes>
-      </>
+    </>
   );
 }
 
